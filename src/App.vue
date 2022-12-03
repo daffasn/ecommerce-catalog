0c2448b9aa778db646ceabdb7e96dffc11ab0c9f
Your Future <daffaseptian24@gmail.com>
2022-12-03 10:46:19 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="app">
+        <ProductView />
+    </div>
+</template>
+
+<script>
+import ProductView from './views/ProductView.vue';
+
+export default {
+    name: 'App',
+    components: {
+        ProductView,
+    },
+};
+</script>
